f7b2726076d4e2276b9884aa4319fe889c85c6cc
Sajjad177 <sajjadhossainx06@gmail.com>
2026-03-15 03:53:54 +0600

    apply job api added

---
+++ src/modules/applyJob/applyJob.model.ts
@@ -0,0 +1,35 @@
+import { model, Schema } from "mongoose";
+import { IApplyJob } from "./applyJob.interface";
+
+const applyJobSchema = new Schema<IApplyJob>(
+  {
+    jobId: {
+      type: Schema.Types.ObjectId,
+      ref: "Job",
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    coverLetter: String,
+    resume: {
+      url: String,
+      public_id: String,
+    },
+    portfolioUrl: String,
+    linkedinUrl: String,
+    status: {
+      type: String,
+      enum: ["pending", "reviewing", "shortlisted", "rejected", "accepted"],
+      default: "pending",
+    },
+    appliedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true, versionKey: false },
+);
+
+const ApplyJob = model<IApplyJob>("ApplyJob", applyJobSchema);
+export default ApplyJob;
