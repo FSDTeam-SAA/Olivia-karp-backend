0127338305f1ee5acada7bda72e67693dc25a077
Sajjad177 <sajjadhossainx06@gmail.com>
2026-03-18 05:28:26 +0600

    course api created

--- src/modules/course/course.controller.ts
+++ src/modules/course/course.controller.ts
@@ -1,6 +1,20 @@
+import { StatusCodes } from "http-status-codes";
 import catchAsync from "../../utils/catchAsync";
+import sendResponse from "../../utils/sendResponse";
+import courseService from "./course.service";
 
-const CreateNewCourse = catchAsync(async (req, res) => {});
+const CreateNewCourse = catchAsync(async (req, res) => {
+    const files = req.files as Express.Multer.File[];
+
+  const result = await courseService.CreateNewCourse(req.body, files);
+
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: "New course created successfully",
+    data: result,
+  });
+});
 
 const courseController = {
   CreateNewCourse,
--- src/modules/course/course.router.ts
+++ src/modules/course/course.router.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
-
-
+import { upload } from "../../middleware/multer.middleware";
+import courseController from "./course.controller";
 
 const router = Router();
 
-
+router.post(
+  "/create",
+  upload.fields([{ name: "image", maxCount: 5 }]),
+  courseController.CreateNewCourse,
+);
 
 const courseRouter = router;
 export default courseRouter;
