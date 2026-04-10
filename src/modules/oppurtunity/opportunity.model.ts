c8358c763253e46e3cd2218edb9b264368c57bfd
Nsarkar-xlr8 <knayemlol24@gmail.com>
2026-04-10 19:27:35 +0600

    feat: implement opportunity module with submission, admin retrieval, and status management features

---
+++ src/modules/oppurtunity/opportunity.model.ts
@@ -0,0 +1,24 @@
+import { model, Schema } from "mongoose";
+import { IOpportunity } from "./opportunity.intrface";
+
+
+const opportunitySchema = new Schema<IOpportunity>(
+    {
+        title: { type: String, required: true },
+        organizationName: { type: String, required: true },
+        opportunityType: { type: String, required: true },
+        location: { type: String, required: true },
+        officialLink: { type: String, required: true },
+        shortDescription: { type: String, required: true, maxlength: 300 },
+        submittedBy: { type: Schema.Types.ObjectId, ref: "User", required: true },
+        status: {
+            type: String,
+            enum: ["pending", "reviewed", "rejected"],
+            default: "pending",
+        },
+    },
+    { timestamps: true, versionKey: false }
+);
+
+const Opportunity = model<IOpportunity>("Opportunity", opportunitySchema);
+export default Opportunity;
